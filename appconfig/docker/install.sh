dd24fdc8a59318b99030f84702aa593cb45318c0
Ondrej Prochazka <mroproch@gmail.com>
2024-01-27 12:52:34 +0100

    Fixed install script for docker.

--- appconfig/docker/install.sh
+++ appconfig/docker/install.sh
@@ -5,25 +5,27 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
-
 default=y
-[[ -t 0 ]] && { read -t 10 -n 2 -p $'\e[1;32mInstall docker? [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
+while true; do
+  if [[ "$unattended" == "1" ]]
+  then
+    resp=$default
+  else
+    [[ -t 0 ]] && { read -t 10 -n 2 -p $'\e[1;32mInstall docker? [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
+  fi
+  response=`echo $resp | sed -r 's/(.*)$/\1=/'`
 
-response=`echo $resp | sed -r 's/(.*)$/\1=/'`
+  if [[ $response =~ ^(y|Y)=$ ]]
+  then
 
+	toilet Installing docker
+	curl -sSL https://get.docker.com | sh
 
-if [[ $response =~ ^(y|Y)=$ ]]
-then
-
-toilet Installing docker
-
-curl -sSL https://get.docker.com | sh
-
-elif [[ $response =~ ^(n|N)=$ ]]
-then
-	echo " ..."
-	break
-else
-	echo " What? \"$resp\" is not a correct answer. Try y+Enter."
-fi
-
+    break
+  elif [[ $response =~ ^(n|N)=$ ]]
+  then
+    break
+  else
+    echo " What? \"$resp\" is not a correct answer. Try y+Enter."
+  fi
+done
